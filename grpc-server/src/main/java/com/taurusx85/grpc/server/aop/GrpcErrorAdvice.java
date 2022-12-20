9ac359a5c3e05b55f7bb243255eb8d4be33f29e1
Oleg <taurusx85@gmail.com>
2022-12-20 15:15:50 +0200

    Error advice - Change Status and Code from Google to io.grpc

--- grpc-server/src/main/java/com/taurusx85/grpc/server/aop/GrpcErrorAdvice.java
+++ grpc-server/src/main/java/com/taurusx85/grpc/server/aop/GrpcErrorAdvice.java
@@ -1,8 +1,8 @@
 package com.taurusx85.grpc.server.aop;
 
 import com.google.rpc.Code;
-import com.google.rpc.Status;
 import com.taurusx85.grpc.server.exception.EntityNotFoundException;
+import io.grpc.Status;
 import net.devh.boot.grpc.server.advice.GrpcAdvice;
 import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
 
@@ -12,9 +12,7 @@ public class GrpcErrorAdvice {
 
     @GrpcExceptionHandler
     public Status handleEntityNotFoundException(EntityNotFoundException e) {
-        return Status.newBuilder()
-                     .setCode(Code.NOT_FOUND.getNumber())
-                     .setMessage(e.getMessage())
-                     .build();
+        return Status.fromCode(Status.Code.NOT_FOUND)                           // io.grpc.Status  &  io.grpc.Status.Code
+                     .withDescription(e.getMessage());
     }
 }
